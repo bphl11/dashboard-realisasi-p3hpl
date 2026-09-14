767a0081ac3a1f19b178a0700befd4c358af4d41
sibphl11 <asterales.niza@gmail.com>
2026-09-14 17:54:19 +0800

    Tetapkan DATA_APLIKASI sebagai satu-satunya sumber data manual

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,12 +1,5 @@
 const CONFIG = {
-
-    SHEET_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vShdaPwws12pkv75bkQJL9AYjuC_4xjvANknmsoT6HVmgKeQ2DJsLLm5QzbvlKQJeQvqNGzYALsOk5n/pub?gid=1473286966&single=true&output=csv",
-
-    // Endpoint Google Apps Script untuk membaca transaksi INPUT_REALISASI.
-    INPUT_REALISASI_URL: [
-        "https://script.google.com/macros/s",
-        "AKfycbxxncp8pn5sF5pGOzErDG2vmHiDWfR0R3_m9QXRUXxfb41R9MqbkLyRmMiE98CmIeth7Q",
-        "exec"
-    ].join("/")
-
+    // DATA_APLIKASI adalah satu-satunya sumber data aplikasi.
+    // Perubahan realisasi dilakukan manual langsung pada Google Sheet.
+    SHEET_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vShdaPwws12pkv75bkQJL9AYjuC_4xjvANknmsoT6HVmgKeQ2DJsLLm5QzbvlKQJeQvqNGzYALsOk5n/pub?gid=1473286966&single=true&output=csv"
 };
