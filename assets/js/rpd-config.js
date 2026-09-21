a909cedd9c8aabb90b76f8ad7b98d1f66d325592
sibphl11 <asterales.niza@gmail.com>
2026-09-21 17:34:07 +0800

    Configure RPD Apps Script Web App URL

--- assets/js/rpd-config.js
+++ assets/js/rpd-config.js
@@ -9,7 +9,7 @@ const RPD_CONFIG = {
     GOOGLE_CLIENT_ID: "443412026871-pqoa9tskrfkaffp5u2ohjhtq1l0ds2r1.apps.googleusercontent.com",
 
     // Isi dengan URL Web App Google Apps Script setelah deploy.
-    RPD_API_URL: "",
+    RPD_API_URL: "https://script.google.com/macros/s/AKfycbxxncp8pn5sF5pGOzErDG2vmHiDWfR0R3_m9QXRUXxfb41R9MqbkLyRmMiE98CmIeth7Q/exec",
 
     // Nama aplikasi/scope sederhana untuk tampilan login.
     APP_NAME: "RPD P3HPL"
